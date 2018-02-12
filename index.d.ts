fbd5afbb37c84fe2d3e5c40a641ff2c4ff356440
宋鹏 <songpeng02@meituan.com>
2018-02-12 17:59:15 +0800

    update d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,10 @@
 // Type definitions for BMA
+export as namespace eslintInit;
 export enum CiSolution {
   none = 1,
   husky,
   mfe
-};
+}
 
 export interface DepConfig {
   [index: string]: string
